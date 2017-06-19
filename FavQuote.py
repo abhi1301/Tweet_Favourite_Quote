c6b97767b3e7438eb40efa7126fbc51e6ff396e5
abhi1301 <abhishek.kumar4444@gmail.com>
2017-06-20 00:50:21 +0530

    Added functionality to create image out of a string quote and then post on timeline

--- FavQuote.py
+++ FavQuote.py
@@ -3,10 +3,12 @@ Posting my favourites quotes on my twitter page as images.
 '''
 
 from TwitterAPI import TwitterAPI
+from PIL import Image, ImageDraw, ImageFont
+import textwrap
 
 
 TWEET_TEXT = 'Quote of the day:'
-IMAGE_PATH = 'upload.jpg'
+# IMAGE_PATH = 'upload.jpg'
 
 # Credentials -------------------
 CONSUMER_KEY = 'M3ExtQJiDqipGXaRDRiPdn0fi'
@@ -21,9 +23,42 @@ api = TwitterAPI(CONSUMER_KEY,
                  ACCESS_TOKEN_SECRET)
 
 
+Quote = 'Becoming a dad means you have to be a role model for your son and be someone he can look up to.'
+para = textwrap.wrap(Quote, width=50)
+
+MAX_W, MAX_H = 400, 300
+im = Image.new('RGB', (MAX_W, MAX_H), (0, 0, 0, 0))
+draw = ImageDraw.Draw(im)
+current_h, pad = 30, 10
+# font = ImageFont.truetype('Arial.ttf', 18)
+
+for line in para:
+    w, h = draw.textsize(line)#, font=font)
+    print(w,'   ',h)
+    draw.text(((MAX_W - w) / 2, current_h), line)#, font=font)
+    current_h += h + pad
+
+im.save('test.png')
+
+# --------------------------------------------------------------------
+# with Drawing() as draw:
+#     with Image(width=600, height=300, background=Color('white')) as image:
+#         # draw.font = 'wandtests/assets/League_Gothic.otf'
+#         draw.font_family = 'monospace'
+#         draw.font_size = 25
+#         metrics = draw.get_font_metrics(image,"How BIG am I?", multiline=False)
+#         print(metrics)
+#
+#         # draw.font_size = 40
+#         # draw.text(int(image.width / 2), int(image.height / 2),TEXT_ALIGN_TYPES='center', 'Hello, world!')
+#         # draw.text(text_alignment()
+#         draw(image)
+#         image.save(filename='200x100-transparent.png')
+# ?_________________________________________________________________________________________________________-
+
 
 # STEP 1 - upload image
-file = open(IMAGE_PATH, 'rb')
+file = open('test.png', 'rb')
 data = file.read()
 
 r = api.request('media/upload', None, {'media': data})
