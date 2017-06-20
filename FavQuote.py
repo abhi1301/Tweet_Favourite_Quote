9b38925c5b32fbb5e458dd3ddf6e97a7bd9b43bb
abhi1301 <abhishek.kumar4444@gmail.com>
2017-06-20 12:53:19 +0530

    Fix : large text out of bounds of the image- dynamic image sizing

--- FavQuote.py
+++ FavQuote.py
@@ -25,11 +25,19 @@ api = TwitterAPI(CONSUMER_KEY,
 
 Quote = 'Becoming a dad means you have to be a role model for your son and be someone he can look up to.'
 para = textwrap.wrap(Quote, width=50)
+lines = 0
+for line in para:
+    lines+=1
 
+# print(para[1])
 MAX_W, MAX_H = 400, 300
 im = Image.new('RGB', (MAX_W, MAX_H), (0, 0, 0, 0))
 draw = ImageDraw.Draw(im)
 current_h, pad = 30, 10
+w, h = draw.textsize(para[0])
+MAX_H = (h+pad)*lines + 80
+im = Image.new('RGB', (MAX_W, MAX_H), (0, 0, 0, 0))
+draw = ImageDraw.Draw(im)
 # font = ImageFont.truetype('Arial.ttf', 18)
 
 for line in para:
@@ -57,18 +65,18 @@ im.save('test.png')
 # ?_________________________________________________________________________________________________________-
 
 
-# STEP 1 - upload image
-file = open('test.png', 'rb')
-data = file.read()
-
-r = api.request('media/upload', None, {'media': data})
-print(r.status_code)
-
-print('UPLOAD MEDIA SUCCESS' if r.status_code == 200 else 'UPLOAD MEDIA FAILURE')
-
-# STEP 2 - post tweet with a reference to uploaded image
-if r.status_code == 200:
-    media_id = r.json()['media_id']
-    r = api.request(
-        'statuses/update', {'status': TWEET_TEXT, 'media_ids': media_id})
-    print('UPDATE STATUS SUCCESS' if r.status_code == 200 else 'UPDATE STATUS FAILURE')
+# # STEP 1 - upload image
+# file = open(IMAGE_PATH, 'rb')
+# data = file.read()
+#
+# r = api.request('media/upload', None, {'media': data})
+# print(r.status_code)
+#
+# print('UPLOAD MEDIA SUCCESS' if r.status_code == 200 else 'UPLOAD MEDIA FAILURE')
+#
+# # STEP 2 - post tweet with a reference to uploaded image
+# if r.status_code == 200:
+#     media_id = r.json()['media_id']
+#     r = api.request(
+#         'statuses/update', {'status': TWEET_TEXT, 'media_ids': media_id})
+#     print('UPDATE STATUS SUCCESS' if r.status_code == 200 else 'UPDATE STATUS FAILURE')
